e7a1e37a9648d6a1cd6974b0fbb1ad274c356754
fathuur7 <230411100035@student.trunojoyo.ac.id>
2025-04-26 20:37:01 +0700

    fix(server.js): remove unnecessary URL from server start log message

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use((err, req, res, next) => {
 // Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 export default app;
